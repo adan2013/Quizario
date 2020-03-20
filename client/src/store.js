d1d8c966b35830f023bb7236327fc530213b31bf
Daniel Alberski <danielalberski@gmail.com>
2020-03-20 22:06:04 +0100

    load/save store function removed

--- client/src/store.js
+++ client/src/store.js
@@ -2,42 +2,14 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk'
 import reducers from './reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const loadState = () => {
-    try {
-        const serializedState = window.localStorage.getItem('state');
-        if (serializedState === null) {
-            return undefined;
-        }
-        return JSON.parse(serializedState);
-    } catch (err) {
-        return undefined;
-    }
-};
-
-const saveState = (state) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        window.localStorage.setItem('state', serializedState);
-    } catch {
-        // ignore write errors
-    }
-};
-
-const prevState = loadState();
 const store = createStore(
     reducers,
-    prevState,
+    {},
     composeEnhancers(
         applyMiddleware(thunk)
     )
 );
-
-store.subscribe(() => {
-    saveState({
-        game: store.getState().game
-    });
-});
 
 export default store
