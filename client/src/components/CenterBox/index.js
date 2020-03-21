fbfa41fc0ac5c9878d4e18bd8c6514c7270b6c37
Daniel Alberski <danielalberski@gmail.com>
2020-03-21 20:39:49 +0100

    cancel btn & logo watermark

--- client/src/components/CenterBox/index.js
+++ client/src/components/CenterBox/index.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
+import {Button} from "react-bootstrap";
 import './CenterBox.css'
+
+import logo from '../../assets/logo.svg'
+import CloseIcon from '@material-ui/icons/Close'
 
 class CenterBox extends Component {
     render() {
@@ -10,6 +14,20 @@ class CenterBox extends Component {
                         {this.props.children}
                     </div>
                 </div>
+                {
+                    this.props.cancel &&
+                    <div className={"cancel-btn"}>
+                        <Button variant={"secondary"} onClick={() => this.props.history.push('/')}>
+                            <CloseIcon/> Anuluj
+                        </Button>
+                    </div>
+                }
+                {
+                    this.props.logo &&
+                    <div className={"watermark-logo"}>
+                        <img src={logo} alt={"quizario watermark logo"}/>
+                    </div>
+                }
             </div>
         );
     }
