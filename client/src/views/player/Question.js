255657ab69c40bc99c1b08595a3229ba14f95ca7
Daniel Alberski <danielalberski@gmail.com>
2020-03-23 10:46:45 +0100

    new player keyboard layout

--- client/src/views/player/Question.js
+++ client/src/views/player/Question.js
@@ -13,57 +13,29 @@ class Question extends Component {
     Keyboard = () => {
         return(
             <div>
-                <Row noGutters className={"keyboard-pc"}>
-                    <Col xs={3}>
+                <Row noGutters>
+                    <Col md={3} xs={6}>
                         <Button variant={"secondary"}
-                                className={"keyboard-button"}
+                                className={"keyboard-button keyboard-left-column"}
                                 onClick={() => this.selectAnswer(0)}>
                             <img src={answerA} alt={"answer A"}/>
                         </Button>
                     </Col>
-                    <Col xs={3}>
+                    <Col md={3} xs={6}>
                         <Button variant={"secondary"}
                                 className={"keyboard-button"}
                                 onClick={() => this.selectAnswer(1)}>
                             <img src={answerB} alt={"answer B"}/>
                         </Button>
                     </Col>
-                    <Col xs={3}>
+                    <Col md={3} xs={6}>
                         <Button variant={"secondary"}
-                                className={"keyboard-button"}
+                                className={"keyboard-button keyboard-left-column"}
                                 onClick={() => this.selectAnswer(2)}>
                             <img src={answerC} alt={"answer C"}/>
                         </Button>
                     </Col>
-                    <Col xs={3}>
-                        <Button variant={"secondary"}
-                                className={"keyboard-button"}
-                                onClick={() => this.selectAnswer(3)}>
-                            <img src={answerD} alt={"answer D"}/>
-                        </Button>
-                    </Col>
-                </Row>
-                <Row noGutters className={"keyboard-mobile"}>
-                    <Col xs={6}>
-                        <div className={"keyboard-left-column"}>
-                            <Button variant={"secondary"}
-                                    className={"keyboard-button"}
-                                    onClick={() => this.selectAnswer(0)}>
-                                <img src={answerA} alt={"answer A"}/>
-                            </Button>
-                            <Button variant={"secondary"}
-                                    className={"keyboard-button"}
-                                    onClick={() => this.selectAnswer(2)}>
-                                <img src={answerC} alt={"answer C"}/>
-                            </Button>
-                        </div>
-                    </Col>
-                    <Col xs={6}>
-                        <Button variant={"secondary"}
-                                className={"keyboard-button"}
-                                onClick={() => this.selectAnswer(1)}>
-                            <img src={answerB} alt={"answer B"}/>
-                        </Button>
+                    <Col md={3} xs={6}>
                         <Button variant={"secondary"}
                                 className={"keyboard-button"}
                                 onClick={() => this.selectAnswer(3)}>
