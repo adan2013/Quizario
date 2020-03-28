4aa7852e249d42a330cf0390b8723740f9cecb83
Daniel Alberski <danielalberski@gmail.com>
2020-03-28 12:02:10 +0100

    questions upload in host view

--- client/src/views/host/Creating.js
+++ client/src/views/host/Creating.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import CenterBox from "../../components/CenterBox";
 import {Button, Container, Row, Col, Form} from "react-bootstrap";
-import testQuestions from "../../testQuestions";
 import {createNewRoom} from "../../connection/config";
 import './Creating.css';
 import TimePicker from "../../components/TimePicker";
@@ -9,16 +8,20 @@ import NumberPicker from "../../components/NumberPicker";
 import LogicSwitch from "../../components/LogicSwitch";
 
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PublishIcon from '@material-ui/icons/Publish';
+import {validateJson} from "../../utilities";
 
 class Creating extends Component {
     constructor(props) {
         super(props);
         this.state = {
             title: '',
+            questions: [],
             timeLimit: 0,
             questionLimit: 0,
             randomOrder: false
-        }
+        };
+        this.inputFile = React.createRef();
     }
 
     shuffle = (array) => {
@@ -40,12 +43,41 @@ class Creating extends Component {
             questionLimit: this.state.questionLimit,
             randomOrder: this.state.randomOrder
         };
-        let q = testQuestions;
+        let q = this.state.questions.slice();
         if(this.state.randomOrder) this.shuffle(q);
         this.props.questionList(q);
         this.props.setHostingRoom(data);
         this.props.switchState('WAITING_FOR_CODE');
         this.props.socket.emit(createNewRoom, data);
+    };
+
+    uploadFile = () => {
+        let fr = new FileReader();
+        fr.onload = (e) => {
+            let output = null;
+            try {
+                output = JSON.parse(e.target.result);
+                if(validateJson(output)) {
+                    this.setState({
+                        questions: output
+                    });
+                }else{
+                    this.setState({
+                        questions: []
+                    });
+                    alert('Wykryto błędną strukturę pliku!');
+                }
+            }catch(error) {
+                this.setState({
+                    questions: []
+                });
+                alert('Wykryto błędną składnię pliku JSON!');
+            }
+            this.inputFile.current.value = "";
+        };
+        if(this.inputFile.current.files.item(0)) {
+            fr.readAsText(this.inputFile.current.files.item(0));
+        }
     };
 
     render() {
@@ -55,34 +87,52 @@ class Creating extends Component {
                     <form>
                         <Container>
                             <Row>
-                                <Col md={6}>
-                                    <div className={"creating-label"}>Tytuł quizu:</div>
-                                    <Form.Control type={"text"}
-                                                  value={this.state.title}
-                                                  className={"creating-textbox"}
-                                                  onChange={(e) => this.setState({title: e.target.value.trim()})}
-                                                  maxLength={"30"}/>
-                                    <div className={"creating-label"}>Kolejność pytań:</div>
-                                    <LogicSwitch value={this.state.randomOrder}
-                                                 offText={"Zwykła"} onText={"Losowa"}
-                                                 onChange={(e) => this.setState({randomOrder: e})}/>
+                                <Col md={6} className={"vcenter"}>
+                                    <div>
+                                        <div className={"creating-label"}>Tytuł quizu:</div>
+                                        <Form.Control type={"text"}
+                                                      value={this.state.title}
+                                                      className={"creating-textbox"}
+                                                      onChange={(e) => this.setState({title: e.target.value.trim()})}
+                                                      maxLength={"30"}/>
+                                        <div className={"creating-label"}>Baza pytań:</div>
+                                        <span className={"btn btn-secondary btn-file"}>
+                                            <PublishIcon/> Wybierz plik z pytaniami
+                                            <input type={"file"} accept={"application/json"} onChange={this.uploadFile} ref={this.inputFile}/>
+                                        </span>
+                                        <div className={"questions-info"}>
+                                            {
+                                                this.state.questions.length === 0
+                                                ?
+                                                    'Brak bazy pytań'
+                                                    :
+                                                    'Załadowano pytań: ' + this.state.questions.length
+                                            }
+                                        </div>
+                                    </div>
                                 </Col>
-                                <Col md={6}>
-                                    <div className={"creating-label"}>Limit czasu:</div>
-                                    <TimePicker value={this.state.timeLimit}
-                                                  min={0} max={300} zeroText={"Wył."}
-                                                  onChange={(e) => this.setState({timeLimit: e})}/>
-                                    <div className={"creating-label"}>Limit ilości pytań:</div>
-                                    <NumberPicker value={this.state.questionLimit}
-                                                  min={0} max={500} zeroText={"Wył."}
-                                                  onChange={(e) => this.setState({questionLimit: e})}/>
+                                <Col md={6} className={"vcenter"}>
+                                    <div>
+                                        <div className={"creating-label"}>Kolejność pytań:</div>
+                                        <LogicSwitch value={this.state.randomOrder}
+                                                     offText={"Zwykła"} onText={"Losowa"}
+                                                     onChange={(e) => this.setState({randomOrder: e})}/>
+                                        <div className={"creating-label"}>Limit czasu:</div>
+                                        <TimePicker value={this.state.timeLimit}
+                                                    min={0} max={300} zeroText={"Wył."}
+                                                    onChange={(e) => this.setState({timeLimit: e})}/>
+                                        <div className={"creating-label"}>Limit ilości pytań:</div>
+                                        <NumberPicker value={this.state.questionLimit}
+                                                      min={0} max={500} zeroText={"Wył."}
+                                                      onChange={(e) => this.setState({questionLimit: e})}/>
+                                    </div>
                                 </Col>
                             </Row>
                         </Container>
                         <Button type={"submit"}
                                 variant={"secondary"}
                                 onClick={this.createRoom}
-                                disabled={this.state.title.trim() === ''}
+                                disabled={this.state.title.trim() === '' || this.state.questions.length === 0}
                                 className={"creating-button"}>
                             <PlayCircleOutlineIcon/><br/>
                             Utwórz pokój
